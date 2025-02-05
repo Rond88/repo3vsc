cb826371c2e47bba66e05765914626c96fa7a6de
Rond88 <r0nald19202006@gmail.com>
2025-02-05 13:24:12 +0100

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World3!");
+        System.out.println("Goodbye, World3!");
     }
 }
